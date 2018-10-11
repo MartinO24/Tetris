52cefd01e2e2930383de980a4ceef35a448db0f7
MartinO24 <35564657+MartinO24@users.noreply.github.com>
2018-10-11 13:39:37 +0200

    Create InputHelper.cs

---
+++ InputHelper.cs
@@ -0,0 +1,44 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Tetris.Classes
+{
+    class InputHelper
+
+    {
+        protected Vector2 scale, offset;
+        KeyboardState currentKeyboardState, previousKeyboardState;
+
+        public InputHelper()
+        {
+            scale = Vector2.One;
+            offset = Vector2.Zero;
+        }
+
+        public void Update()
+        {
+            previousKeyboardState = currentKeyboardState;
+            currentKeyboardState = Keyboard.GetState();
+        }
+        public Vector2 Scale
+        {
+            get { return scale; }
+            set { scale = value; }
+        }
+
+        public Vector2 Offset
+        {
+            get { return offset; }
+            set { offset = value; }
+        }
+        public bool KeyPressed(Keys k)
+        {
+            return currentKeyboardState.IsKeyDown(k) && previousKeyboardState.IsKeyUp(k);
+        }
+
+        public bool KeyDown(Keys k)
+        {
+            return currentKeyboardState.IsKeyDown(k);
+        }
+    }
+}
