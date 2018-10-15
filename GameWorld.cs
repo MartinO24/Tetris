b29b44bda5d1770bab39e7d715930f54611dccd8
Pelac <35537606+Pelac@users.noreply.github.com>
2018-10-15 14:38:52 +0200

    Update GameWorld.cs

--- GameWorld.cs
+++ GameWorld.cs
@@ -10,120 +10,46 @@ using Tetris.Classes;
 using Microsoft.Xna.Framework.Content;
 
 namespace Tetris.Classes
-{
-    class Block
+{ 
+    class GameWorld
     {
-        float elapsedGameTime = 0.0f;
-
-        TetrisGrid tetrisGrid;
-
-        Color color;
-        protected Point position;
-        protected Texture2D emptyCell;
-        protected bool[,] shape;
-        Random rnd;
-        ZBlock zBlock;
-        TBlock tBlock;
-        SquBlock squBlock;
-        LBlockInvert lBlockInvert;
-        LBlock lBlock;
-        Block activeblock;
-        CurrentBlock currentblock;
-        IBlock iblock;
-
-        public Block (ContentManager Content, Color color)
+    enum GameState
+    {
+        Playing,
+        GameOver
+    }
+    public static Random Random { get { return Random; } }
+    static Random random;
+    GameState gameState;
+    SpriteFont font;
+    TetrisGrid grid;
+    public GameWorld(ContentManager Content)
+    {
+        random = new Random();
+        gameState = GameState.Playing;
+        font = Content.Load<SpriteFont>("SpelFont");
+        grid = new TetrisGrid(Content);
+    }
+        public void HandleInput(GameTime gameTime, InputHelper inputHelper)
         {
-            this.color = color;
-            position = new Point(3, 0);
-            emptyCell = Content.Load<Texture2D>("block");
-            tetrisGrid = new TetrisGrid(Content);
-            rnd = new Random();
-        }      
-        public Block SpawnNewBlock(ContentManager Content)
-        {
-            LBlockInvert block = new LBlockInvert(Content);
-            return block;
-            /*
-            int rndBlock = rnd.Next(0,2);
-            if (rndBlock == 0)
-            {
-                block = new ZBlock(Content);
-                return block;
-            }
-            else if (rndBlock == 1)
-            {
-                block = new SquBlock(Content);
-                return block;
-            }
-            else
-            {
-                block = new TBlock(Content);
-                return block;
-            }   */         
         }
-        public void TurnLeft(ContentManager Content, Block block)
+       
+        public void Update(GameTime gameTime)
         {
-            lBlockInvert = new LBlockInvert(Content);
-            shape = lBlockInvert.RotatationLeft1();
-        }
-        public void TurnLeft2(ContentManager Content, Block block)
-        {
-            //lBlockInvert = new LBlockInvert(Content);
-            shape = lBlockInvert.RotatationLeft2();
-        }
-        public void TurnRight(ContentManager Content, Block block)
-        {
-            lBlockInvert = new LBlockInvert(Content);
-            shape = lBlockInvert.RotatationRight1();
-        }
-        public void TurnRight2(ContentManager Content, Block block)
-        {
-            lBlockInvert = new LBlockInvert(Content);
-            shape = lBlockInvert.RotatationRight2();
-        }
-        public void MoveLeft()
-        {
-            if (tetrisGrid.InsideGrid(position))
-                position.X--;
-            else
-                position.X++;
-        }
-        public void MoveRight()  //Kan ett block vara på denna positionen innanför gridden
-        {
-            if (tetrisGrid.InsideGrid(position))
-                position.X++;
-            else
-                position.X = 1;
-        }
-        public void MoveDown()
-        {
-            if (tetrisGrid.InsideGrid(position))
-                position.Y++;
-            else
-                position.Y--;
-        }
-        public void Falling(GameTime gameTime)
-        {
-            elapsedGameTime += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
-            if (elapsedGameTime >= 1000.0f)
-            {
-                position.Y++;
-                elapsedGameTime = 0;
-            }
         }
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            for (int x = 0; x < 4; x++)
-            {
-                for (int y = 0; y < 4; y++)
-                {
-                    if(shape[x,y])
-                    {
-                        Vector2 drawPosition = new Vector2((position.X + y) * emptyCell.Width, (position.Y + x) * emptyCell.Height);
-                        spriteBatch.Draw(emptyCell, drawPosition, color);
-                    }
-                }
-            }
-        } 
+            Vector2 scorePosition = new Vector2(330, 0);
+            Vector2 levelPosition = new Vector2(330, 30);
+            Vector2 nextBlockPosition = new Vector2(330, 60);
+
+            spriteBatch.DrawString(font, "Score: ", scorePosition, Color.Blue);
+            spriteBatch.DrawString(font, "Level: ", levelPosition, Color.Blue);
+            spriteBatch.DrawString(font, "Next block: ", nextBlockPosition, Color.Blue);
+        }
+        public void Reset()
+        {
+        }
     }
+     
 }
