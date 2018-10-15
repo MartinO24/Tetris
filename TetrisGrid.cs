8eb425e77453575b3ff3a41ac08ac180ff615f43
Pelac <35537606+Pelac@users.noreply.github.com>
2018-10-15 21:49:20 +0200

    Update TetrisGrid.cs

--- TetrisGrid.cs
+++ TetrisGrid.cs
@@ -16,47 +16,83 @@ namespace Tetris.Classes
         protected int layer;
         protected Matrix spriteScale;
         Texture2D emptyCell;
-        
+        Block block;
+        Color color = Color.White;
         Vector2 position;
-
-        int[,] grid;
-
-
+        Vector2 blockPosition;
+        //LBlockInvert lBlockInvert;
+        bool[,] grid;
+        bool[,] shape;
+        
         public int Width { get { return 10; } }
 
         public int Height { get { return 20; } }
-        
 
-        public TetrisGrid(ContentManager Content, int layer = 0)
+        public TetrisGrid(ContentManager Content)
         {
-            this.layer = layer;
             // Create a new SpriteBatch, which can be used to draw textures.
             emptyCell = Content.Load<Texture2D>("block");
             //här skapas Gridden 10*20
-            grid = new int[10, 20];
-            for (int x = 0; x < 5; x++)
-                for (int y = 0; y < 10; y++)
-                    grid[x, y] = 0;
-            Clear();
+            grid = new bool[Width, Height];
+            //Clear();
         }
-
+        public void GridBlock () //Should take in a shape from block
+        {
+            shape = new bool[,]
+            {
+                {false, false, true, false},
+                {false, false, true, false},
+                {false, true, true, false},
+                {false, false, false, false},
+            };
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    grid[y + 3, x] = shape[x, y];
+                }
+            }
+        }
+        public void Update(GameTime gameTime)
+        {
+            GridBlock();
+        }
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            for (int x = 0; x < 10; x++)
+            for (int x = 0; x < Width; x++)
             {
-                for (int y = 0; y < 20; y++)
+                for (int y = 0; y < Height; y++)
                 {
-                    Vector2 position = new Vector2(0 + x * 30, 0 + y * 30);
-                    if (grid[x, y] == 0)
+                    Vector2 position = new Vector2(0 + x * emptyCell.Width, 0 + y * emptyCell.Height);
+                    if (grid[x, y] == false)
                     {
                         spriteBatch.Draw(emptyCell, position, Color.White);
+                    }
+                    else if (grid[x, y])
+                    {
+                        blockPosition = new Vector2((0 + x) * emptyCell.Width,(0 + y) * emptyCell.Height);
+                        spriteBatch.Draw(emptyCell, blockPosition, Color.Green);
                     }
                 }
             }
         }
-        
-        public void Clear()
+     /*   public void Clear()
         {
+            for (int x = 0; x < 5; x++)
+                for (int y = 0; y < 10; y++)
+                    grid[x, y] = 0;
+        } */
+        public bool InsideGrid(Point position)
+        {
+            return position.X >= 0 && position.X < Width && position.Y < Height && position.Y >= 0;
+        }
+        public bool ShapeInsideGrid (bool[,]shape)
+        {
+            if (shape == grid)
+                return true;
+            else
+                return false;
         }
     }
 }
+
